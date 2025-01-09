a14a1c9d9b0537441058afbbabf32080edd27972
fahizakasim <fahizakasim@gmail.com>
2025-01-09 16:10:16 +0530

    Jan8

---
+++ day2/datatype.py
@@ -0,0 +1,10 @@
+x1 = 9
+x2 = 9.0
+x3 = "Hi"
+x4 = 80
+x5 = ['a','b','c'] #list
+x6 = ('a','b','c') #tuple
+x7 = {"name": "fahiza","age" : 1} #dic
+x8 = {'a','b','c','a','b'} #set
+print(x1, x2, x3, x4, len(x5), x5[2], len(x6), x6[1], x7["name"], len(x8))
+
