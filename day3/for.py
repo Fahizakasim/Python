de95eb1620f7324c95d7b37880c2ac48a0ae1001
fahizakasim <fahizakasim@gmail.com>
2025-01-09 20:13:17 +0530

    Jan8

--- day3/for.py
+++ day3/for.py
@@ -1,15 +1,24 @@
-for name in "fahiza":
+#FOR LOOP -- Printing the character of the string.
+for name in "fahiza": 
     print(name)
     
+#FOR LOOP -- Printing the numbers in range(begin, end+1, jump):    
 for name in range (0, 100, 10):
     print(name)
 
+#FOR LOOP -- Printing the numbers in set in sorted order:    
 for name in sorted({'1','2','3','4','5'}):
     print(name)
+
+#FOR LOOP -- Printing the numbers in range(begin, end+1, jump):    
 for i in range(1,101):
     print(i)
+
+ #FOR LOOP -- Printing the numbers in reverse order:   
 for i in range(10,0,-1):
     print(i)
+
+#FOR LOOP -- Nested for loop for i and j:
 
 for i in range(1,4):
     for j in range(2,5):
