ab6d90a78d44a42acad0fba60d3ac423668617d6
fahizakasim <fahizakasim@gmail.com>
2025-01-09 20:08:55 +0530

    jan9

---
+++ day3/Ifcondition.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+num3 = int(input("Enter num3: "))
+if (num1 > num2):
+    print("num1 is greatest: ", num1)
+elif (num2 > num3):
+    print("num2 is greatest: ", num2)
+else:
+    print("num3 is greatest: ", num3)
+
+    
---
+++ day3/for.py
@@ -0,0 +1,16 @@
+for name in "fahiza":
+    print(name)
+    
+for name in range (0, 100, 10):
+    print(name)
+
+for name in sorted({'1','2','3','4','5'}):
+    print(name)
+for i in range(1,101):
+    print(i)
+for i in range(10,0,-1):
+    print(i)
+
+for i in range(1,4):
+    for j in range(2,5):
+        print(i,j)
